ef92c10ea1e772202a9b709fadd7f48c62ce4e0f
Joao Menegasso <joao.menegasso@sap.com>
2024-12-16 14:15:51 -0300

    update resume

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
                     <li className='container__menu-item'><a href='#skills' className='menu-item'>Habilidades</a></li>
                     <li className='container__menu-item'><a href='#projects' className='menu-item'>Projetos</a></li>
                     <li className='container__menu-item'><a href='#info' className='menu-item'>Contato</a></li>
-                    <li className='container__menu-item'><a href='https://raw.githubusercontent.com/joaolucasmm/portfolio/main/src/Assets/Documents/JoaoLucasMirandaMenegasso.pdf' 
+                    <li className='container__menu-item'><a href='https://raw.githubusercontent.com/joaolucasmm/portfolio/main/src/Assets/Documents/JoaoLucasMirandaMenegassoResume.pdf' 
                         className='menu-item' target="_blank" rel="noreferrer">Currículo</a></li>
                     
                 </ul>
