524422827df759d2079a9492b94ba8cdd4129576
Joao Menegasso <joao.menegasso@sap.com>
2025-02-23 16:12:19 -0300

    Create a translation function to avoid the use of multiple files
    The texts.js file was created to keep all the translations in the same place and pass the appropriate text to the Banner component according to the selected language. All the texts were saved in arrays and are selected by position. A better approach may be chosen in future updates.
    
    The Banner component was changed to receive the language passed down by the App file as a prop and send it down the line to the selection text function, along with the text position indicating which text should be exhibit on screen.
    
    The App.js file now only verifies the language in the translate button, instead of the whole app, since the use of files fully translated will be deprecated.
    
    As a temporary step, the App.js is still showing the other components in the old translate files format.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import HeaderEN from './Components/Header_en/Header';
 import Banner from './Components/Banner/Banner';
-import BannerEN from './Components/Banner_en/Banner';
+// import BannerEN from './Components/Banner_en/Banner';
 import Skills from './Components/Skills/Skills';
 import SkillsEN from './Components/Skills_en/Skills';
 import Footer from './Components/Footer/Footer';
@@ -17,49 +17,37 @@ import InfoEN from './Components/Info_en/Info';
 import Retorno from './Components/Retorno/Retorno';
 
 function App() {
-	const [toggle, setToggle] = useState(true);
-	if(toggle){
-		return (
-		  <div className="App">
-			  <Router>
+	const [language, setLanguage] = useState("english");
+	return (
+		<div className="App">
+			<Router>
 				<div className='container__header'>
-					<HeaderEN />
+					{language === "english" ? <HeaderEN /> : <Header />}
 					<div className='container__translate'>
-						<button className='translate' onClick={() => setToggle(false)}>Portuguese</button>
+						<button className='translate' onClick={() => setLanguage(language === "english" ? "portuguese" : "english")}>
+							{language === "english" ? "Portuguese" : "English"}
+						</button>
 					</div>
 				</div>
-			  </Router>
-			  <BannerEN />
-			  <SkillsEN />
-			  <ProjectsEN />
-			  <InfoEN />
-			  <Retorno />
-			  <Footer />
-			  
-		  </div>
-		);
-	}
-	else {
-		return (
-		  <div className="App">
-			  <Router>
-			  <div className='container__header'>
-					<Header />
-					<div className='container__translate'>
-						<button className='translate' onClick={() => setToggle(true)}>English</button>
-					</div>
-				</div>	
-			  </Router>
-			  <Banner />
-			  <Skills />
-			  <Projects />
-			  <Info />
-			  <Retorno />
-			  <Footer />
-			  
-		  </div>
-		);
-	}
+			</Router>
+			<Banner language={language} />
+			{language === "english" ? (
+				<>
+					<SkillsEN />
+					<ProjectsEN />
+					<InfoEN />
+				</>
+			) : (
+				<>
+					<Skills />
+					<Projects />
+					<Info />
+				</>
+			)}
+			<Retorno />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import imagem from '../../Assets/Images/avatar.jpg';
+import textSelector from './texts';
 
-function Banner() {
+function Banner({ language }) {
     return (
         <div className='container-banner'>
             <div className='banner'>
                 <span className='banner__text'>
-                    <h1 className='banner__title'>Olá, eu sou João Lucas</h1>
-                    <h2 className='banner__subtitle'>Desenvolvedor Front End!</h2>
-                    <p className='banner__bio'>Sou estudante de engenharia física desde 2017 na UFRGS.
-                    Comecei a trabalhar com desenvolvimento ainda quando fiz curso técnico em mecatrônica em 2014. 
-                    Em 2022 migrei para o desenvolvimento front end e me tornei desenvolvedor em React. 
-                    Em 2023 comecei um estágio no programa de Rotação de áreas na SAP onde trabalhei como Suporte Técnico, 
-                    Engenheiro de Qualidade e atualmente sou Estágiario de Engenharia de Software.</p>
+                    <h1 className='banner__title'>{textSelector(language, 0)}</h1>
+                    <h2 className='banner__subtitle'>{textSelector(language, 1)}</h2>
+                    <p className='banner__bio'>{textSelector(language, 2)}</p>
                 </span>
 
                 <img className='banner__imagem' src={imagem} alt='Logo Doguito' />
