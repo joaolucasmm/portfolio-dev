354eedd6e277fd33c75febd248f691cb76fa49e2
Joao Menegasso <joao.menegasso@sap.com>
2025-03-01 19:53:26 -0300

    Replace custom banner by MUI components

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import imagem from '../../Assets/Images/avatar.jpg';
 import textSelector from './texts';
+import { Grid2 as Grid, ListItem as Item } from '@mui/material';
 
 function Banner({ language }) {
     return (
-        <div className='container-banner'>
-            <div className='banner'>
-                <span className='banner__text'>
+        <Grid container spacing={2} className='container-banner'>
+            <Grid size={4} className='banner'>
+                <Item className='banner__text'>
                     <h1 className='banner__title'>{textSelector(language, 0)}</h1>
                     <h2 className='banner__subtitle'>{textSelector(language, 1)}</h2>
                     <p className='banner__bio'>{textSelector(language, 2)}</p>
-                </span>
-
-                <img className='banner__imagem' src={imagem} alt='Logo Doguito' />
-            </div>
-        </div>
+                </Item>
+            </Grid>
+            <Grid size={4} className='banner'>
+                <Item><img className='banner__imagem' src={imagem} alt='Logo Doguito' /></Item>
+            </Grid>
+        </Grid>
     );
 };
 
