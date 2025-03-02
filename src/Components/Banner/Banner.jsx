9dd5dad54809078b268d9009582960d809240b10
Joao Menegasso <joao.menegasso@sap.com>
2025-03-01 23:04:51 -0300

    Create a typewriter effect on banner title

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import imagem from '../../Assets/Images/avatar.jpg';
 import textSelector from './texts';
 import { Grid2 as Grid, ListItem as Item } from '@mui/material';
+import Typewriter from 'typewriter-effect';
 
 function Banner({ language }) {
     return (
         <Grid container spacing={2} className='container-banner'>
             <Grid size={4} className='banner'>
                 <Item className='banner__text'>
-                    <h1 className='banner__title'>{textSelector(language, 0)}</h1>
+                    <h1 className='banner__title'>
+                        <Typewriter
+                        options={{
+                            strings: [textSelector(language, 0)],
+                            autoStart: true,
+                            loop: true,
+                            pauseFor: 5000
+                        }}
+                        /></h1>
                     <h2 className='banner__subtitle'>{textSelector(language, 1)}</h2>
                     <p className='banner__bio'>{textSelector(language, 2)}</p>
                 </Item>
