df19b479ffbfe855f22ba215b1d45a45bb9a8bab
Joao Menegasso <joao.menegasso@sap.com>
2025-03-11 14:01:08 -0300

    Change list for grid elements

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,37 +1,68 @@
 import React from 'react';
-import { FaReact, FaJs, FaSass, FaGit, FaHtml5, FaCss3 } from 'react-icons/fa';
+import { FaReact, FaJs, FaSass, FaGit, FaHtml5, FaCss3, FaNode } from 'react-icons/fa';
 import { SiTypescript, SiWebdriverio, SiArduino, SiSap } from "react-icons/si";
 import textSelector from './texts';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    ...theme.applyStyles('dark', {
+        backgroundColor: '#1A2027',
+    }),
+}));
+
 function Skills({ language }) {
     return (
+
         <div id='skills' className='container-skill'>
-            <div className='skill'>  
-            
+            <div className='skill'>
+
                 <h1 className='skill__title'>{textSelector(language, 0)}</h1>
-                <ul className='skill-list'>
-                    <span className='skill-list__span'>
-                        <li className='skill-list__text'><FaHtml5 />HTML</li>
-                        <li className='skill-list__text'><FaCss3 />CSS</li>
-                    </span>
-                    
-                    <span className='skill-list__span'>
-                        <li className='skill-list__text'><FaJs />Javascript</li>
-                        <li className='skill-list__text'><FaReact />ReactJS</li>
-                    </span>
-                    <span className='skill-list__span'>
-                        <li className='skill-list__text'><FaSass />SASS</li>
-                        <li className='skill-list__text'><FaGit />GIT</li>
-                    </span>
-                    <span className='skill-list__span'>
-                        <li className='skill-list__text'><SiWebdriverio />Webdriver.io</li>
-                        <li className='skill-list__text'><SiTypescript />Typescript</li>
-                    </span>
-                    <span className='skill-list__span'>
-                        <li className='skill-list__text'><SiArduino />Arduino</li>
-                        <li className='skill-list__text'><SiSap />CAP</li>
-                    </span>
-                </ul>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={2} className='skill-list'>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><FaHtml5 />HTML</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><FaCss3 />CSS</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><FaJs />Javascript</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><FaReact />ReactJS</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><FaSass />SASS</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><FaGit />GIT</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><SiWebdriverio />Webdriver.io</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><SiTypescript />Typescript</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><SiArduino />Arduino</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><SiSap />CAP</Item>
+                        </Grid>
+                        <Grid size={2} className='skill-list__span'>
+                            <Item className='skill-list__text'><FaNode />NodeJS</Item>
+                        </Grid>
+                    </Grid>
+                </Box>
             </div>
         </div>
     );
