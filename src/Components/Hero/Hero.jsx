9dfb6cb5cc4babfa381d370c54c8e86d8c7013fc
joaolucasmm <joaol_mm@hotmail.com>
2025-03-27 15:14:39 -0300

    Update texts in Hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,65 +1,67 @@
-import React from 'react';
-import image from '../../Assets/Images/avatar.jpg';
-import Typewriter from 'typewriter-effect';
-import Grid from '@mui/material/Grid2';
-import CardContent from '@mui/material/CardContent';
-import CodeIcon from '@mui/icons-material/Code';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import image from "../../Assets/Images/avatar.jpg";
+import Typewriter from "typewriter-effect";
+import Grid from "@mui/material/Grid2";
+import CardContent from "@mui/material/CardContent";
+import CodeIcon from "@mui/icons-material/Code";
+import Typography from "@mui/material/Typography";
 
-import TerminalIcon from '@mui/icons-material/Terminal';
-import DataObjectIcon from '@mui/icons-material/DataObject';
-import SourceIcon from '@mui/icons-material/Source';
-import textSelector from './texts';
-import { Button } from '@mui/material';
+import TerminalIcon from "@mui/icons-material/Terminal";
+import DataObjectIcon from "@mui/icons-material/DataObject";
+import SourceIcon from "@mui/icons-material/Source";
+import textSelector from "./texts";
+import { Button } from "@mui/material";
 
 function Hero({ styleSection, language }) {
     const resumeLink =
-    language === "english"
-        ? "https://raw.githubusercontent.com/joaolucasmm/portfolio/main/src/Assets/Documents/JoaoLucasMirandaMenegassoResume.pdf"
-        : "https://raw.githubusercontent.com/joaolucasmm/portfolio/main/src/Assets/Documents/JoaoLucasMirandaMenegassoCurriculo.pdf";
+        language === "english"
+            ? "https://raw.githubusercontent.com/joaolucasmm/portfolio/main/src/Assets/Documents/JoaoLucasMirandaMenegassoResume.pdf"
+            : "https://raw.githubusercontent.com/joaolucasmm/portfolio/main/src/Assets/Documents/JoaoLucasMirandaMenegassoCurriculo.pdf";
 
     return (
         <div className={styleSection}>
-            <div className='hero_container'>
+            <div className="hero_container">
                 <Grid container spacing={4}>
                     <Grid size={8}>
-                        <div className='text'>
+                        <div className="text">
                             <h1>
                                 <Typewriter
                                     options={{
                                         strings: [textSelector(language, 0)],
                                         autoStart: true,
                                         loop: true,
-                                        pauseFor: 5000
-                                    }} />
+                                        pauseFor: 5000,
+                                    }}
+                                />
                             </h1>
                             <span>
-                                <h2>
-                                    {textSelector(language, 1)}
-                                </h2>
-                                <p>
+                                <h2>{textSelector(language, 1)}</h2>
+                                <h3>
                                     {textSelector(language, 2)}
-                                </p>
+                                </h3>
+                                    <p><b>{textSelector(language, 3)}</b></p>
+                                    <p><i>{textSelector(language, 4)}</i></p>
                             </span>
                         </div>
                     </Grid>
                     <Grid size={4}>
-                        <div className='logo'>
-                            <img src={image} alt='Logo' className='image' />
+                        <div className="logo">
+                            <img src={image} alt="Logo" className="image" />
                         </div>
                     </Grid>
                     <Grid size={4} />
-                    <Grid size={2} className='button'>
-                        <Button href={resumeLink} target='_blank'>Download Resume</Button>
+                    <Grid size={2} className="button">
+                        <Button href={resumeLink} target="_blank">
+                            Download Resume
+                        </Button>
                     </Grid>
-                    <Grid size={2} className='button'>
-                        <Button href="https://google.com" target='_blank'>Contato</Button>
+                    <Grid size={2} className="button">
+                        <Button href="https://google.com" target="_blank">
+                            Contato
+                        </Button>
                     </Grid>
                     <Grid size={4} />
                 </Grid>
-
-
-
             </div>
         </div>
     );
--- src/Components/Hero/texts.js
+++ src/Components/Hero/texts.js
@@ -1,14 +1,24 @@
 import sendMessage from "../../Functions/sendMessage";
 
 const textPortuguese = [
-    "Experiência"
+    "Olá, eu sou João Lucas!",
+    "- Desenvolvedor Fullstack.",
+    "Seu projeto merece mais que linhas de código:",
+    "Precisa de estratégia, usabilidade e tecnologia que entregue valor real. Como Desenvolvedor FullStack, oriento soluções completas – do conceito à publicação – para conectar marcas a seus clientes de forma eficiente.",
+    "Transformando ideias em produtos digitais de alto impacto. Do design à implantação, crio soluções escaláveis, performáticas e centradas na melhor experiência do usuário – gerando resultados para empresas e negócios.",
+    "Baixe meu currículo",
+    "Me envie um e-mail"
 ]
 
 
 const textEnglish = [
     "Hello, I am João Lucas!",
     "- Fullstack Developer.",
-    "I have been studying physics engineering since 2017 at UFRGS. I started working with development when I took a technical course in mechatronics in 2014. In 2022, I switched to front end development and became a React developer. In 2023, I started an internship in the Rotation program at SAP where I worked as Technical Support, Quality Assurance Engineer, and currently I am a Software Engineer Intern."
+    "Your project deserves more than lines of code:",
+    "It needs strategy, usability and technology that delivers real value. As a FullStack Developer, I guide complete solutions – from concept to publication – to connect brands to their customers efficiently.",
+    "Transforming ideas into high-impact digital products. From design to deployment, I create scalable, performative solutions centered on the best user experience – generating results for companies and businesses.",
+    "Download my resume",
+    "Send me a e-mail"
 ]
 
 function textSelector(language, textPosition) {
