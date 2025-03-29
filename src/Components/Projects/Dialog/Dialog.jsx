a5c18ee7f37e4b53fab3c207d0b94dc0ccd0c266
joaolucasmm <joaol_mm@hotmail.com>
2025-03-29 00:21:51 -0300

    Add a full description to each project

--- src/Components/Projects/Dialog/Dialog.jsx
+++ src/Components/Projects/Dialog/Dialog.jsx
@@ -56,16 +56,7 @@ function projectDialog({
                         />
                         <span className="dialog_rows dialog_description">
                             <section>
-                                {fullDescription} Lorem ipsum dolor sit amet,
-                                consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna
-                                aliqua. Ut enim ad minim veniam, quis nostrud
-                                exercitation ullamco laboris nisi ut aliquip ex
-                                ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum
-                                dolore eu fugiat nulla pariatur. Excepteur sint
-                                occaecat cupidatat non proident, sunt in culpa
-                                qui officia deserunt mollit anim id est laborum.
+                                {fullDescription}
                             </section>
                             <section className="dialog_buttons">
                                 <Button
