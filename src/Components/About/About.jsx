5a4f7127458f3713676990a3f0159d8d67d8b525
joaolucasmm <joaol_mm@hotmail.com>
2025-03-29 17:46:05 -0300

    Add dark mode to About component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,23 +5,24 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import textSelector from "./texts";
+import variables from '../UI/variables.scss';
 
-function About({ styleSection, language }) {
+
+function About({ styleSection, language, darkMode }) {
     return (
         <div className={styleSection}>
-            <Card className="container_about">
+            <Card className="container_about" sx={{bgcolor:variables.color1}}>
                 <CardContent className="about_content">
                     <CardHeader title={textSelector(language, 0)} subheader={textSelector(language, 1)} className="about_presentation"/>
-                    <section></section>
                     <section>
                         {textSelector(language, 2)}
                     </section>
                     <section>
-                        <Button onClick={() => window.open("https://www.linkedin.com/in/jo%C3%A3o-lucas-miranda-menegasso/", "_blank")}>
+                        <Button className={"button " + (darkMode && "button_dark")} variant="contained" onClick={() => window.open("https://www.linkedin.com/in/jo%C3%A3o-lucas-miranda-menegasso/", "_blank")}>
                             <GitHubIcon />
                             GitHub
                         </Button>
-                        <Button onClick={() => window.open("https://github.com/joaolucasmm", "_blank")}>
+                        <Button className={"button " + (darkMode && "button_dark")} variant="contained" onClick={() => window.open("https://github.com/joaolucasmm", "_blank")}>
                             <LinkedInIcon />
                             LinkedIn
                         </Button>
