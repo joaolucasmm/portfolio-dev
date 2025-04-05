86cfc6f2cce521beb07a901711e4c20a56c26470
joaolucasmm <joaol_mm@hotmail.com>
2025-04-05 00:38:16 -0300

    Create animations for Project Card

--- src/Components/Projects/ProjectCard/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard/ProjectCard.jsx
@@ -18,6 +18,7 @@ import LinkIcon from "@mui/icons-material/Link";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import textSelector from "../texts";
 import variables from "../../UI/variables.scss";
+import { Button, Fab } from "@mui/material";
 
 function projectDialog({
     projectKey,
@@ -30,40 +31,58 @@ function projectDialog({
     projectLinkedin = "https://www.linkedin.com/in/jo%C3%A3o-lucas-miranda-menegasso/",
     description,
     handleClickOpen,
+    darkMode,
 }) {
     const handleOpenSettings = (projectKey, event) => {
         event.stopPropagation();
-        const settings = document.querySelector(`.project_settings${projectKey}`);
-        settings.style.display = settings.style.display === "none" ? "block" : "none";
+        const settings = document.querySelector(
+            `.project_settings${projectKey}`
+        );
+        settings.style.display =
+            settings.style.display === "none" ? "block" : "none";
     };
 
     const handleCardClick = (projectKey) => {
-        const settings = document.querySelector(`.project_settings${projectKey}`);
+        const settings = document.querySelector(
+            `.project_settings${projectKey}`
+        );
         settings.style.display = "none";
     };
 
     return (
-        <Card sx={{ width: 345, position: 'relative', bgcolor:variables.color1}} onClick={() => handleCardClick(projectKey)}>
-            <CardHeader
-                action={
-                    <IconButton
-                        aria-label="settings"
-                        onClick={(event) => handleOpenSettings(projectKey, event)}
-                    >
-                        <MoreVertIcon />
-                    </IconButton>
-                }
-                title={title}
-                subheader={date}
-            />
-            <nav aria-label="settings" className={"project_settings" + projectKey}>
+        <Card
+            sx={{ width: 345, position: "relative", bgcolor: variables.color1 }}
+            className={"project_card " + (darkMode && "dark")}
+            onClick={() => handleCardClick(projectKey)}
+        >
+            <span className={"project_header " + (darkMode && "dark")}>
+                <Fab
+                    size="small"
+                    aria-label="add"
+                    onClick={(event) => handleOpenSettings(projectKey, event)}
+                    className="project_settings glass-background"
+                >
+                    <MoreVertIcon />
+                </Fab>
+                <h3 className="card_title">{title}</h3>
+                <h4 className="card_subtitle">{date}</h4>
+            </span>
+            <nav
+                aria-label="settings"
+                className={"project_settings" + projectKey}
+            >
                 <List>
                     <ListItem disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
                                 <LinkIcon />
                             </ListItemIcon>
-                            <ListItemText primary={textSelector(language,13)} onClick={() => {window.open(projectURL, "_blank")}}/>
+                            <ListItemText
+                                primary={textSelector(language, 13)}
+                                onClick={() => {
+                                    window.open(projectURL, "_blank");
+                                }}
+                            />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
@@ -71,7 +90,12 @@ function projectDialog({
                             <ListItemIcon>
                                 <GitHubIcon />
                             </ListItemIcon>
-                            <ListItemText primary={textSelector(language,14)} onClick={() => {window.open(projectRepository, "_blank")}}/>
+                            <ListItemText
+                                primary={textSelector(language, 14)}
+                                onClick={() => {
+                                    window.open(projectRepository, "_blank");
+                                }}
+                            />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
@@ -79,7 +103,12 @@ function projectDialog({
                             <ListItemIcon>
                                 <LinkedInIcon />
                             </ListItemIcon>
-                            <ListItemText primary={textSelector(language,15)} onClick={() => {window.open(projectLinkedin, "_blank")}}/>
+                            <ListItemText
+                                primary={textSelector(language, 15)}
+                                onClick={() => {
+                                    window.open(projectLinkedin, "_blank");
+                                }}
+                            />
                         </ListItemButton>
                     </ListItem>
                 </List>
@@ -89,17 +118,24 @@ function projectDialog({
                 height="194"
                 image={image}
                 alt="Screenshot"
+                className="project_image"
             />
             <CardContent>
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                    {description}
-                </Typography>
+                <p className={darkMode && "dark"}>{description}</p>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
+                <IconButton aria-label="info" className="card_icon">
                     <InfoIcon onClick={() => handleClickOpen()} />
                 </IconButton>
-                <IconButton aria-label="share" onClick={() => window.open("https://api.whatsapp.com/send?text=" + projectURL, "_blank")}>
+                <IconButton
+                    aria-label="share"
+                    onClick={() =>
+                        window.open(
+                            "https://api.whatsapp.com/send?text=" + projectURL,
+                            "_blank"
+                        )
+                    }
+                >
                     <ShareIcon />
                 </IconButton>
             </CardActions>
