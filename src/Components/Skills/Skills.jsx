e7bc9a89562ac4c8586429b65fd02cd1a2adbce7
joaolucasmm <joaol_mm@hotmail.com>
2025-04-04 21:17:23 -0300

    Add glass effect to skills display

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -20,73 +20,73 @@ function Skills({ styleSection, language, darkMode }) {
     return (
         <div className={styleSection}>
             <div className="container_skill">
-                    <h1 className="skill__title">
+                    <h1 className={"skill__title " + (darkMode && "skill__title_dark")}>
                         {textSelector(language, 0)}
                     </h1>
                     <div>
                         <Grid container spacing={2} className="skill-list">
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <FaHtml5 />
                                     HTML
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <FaCss3 />
                                     CSS
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <FaJs />
                                     Javascript
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <FaReact />
                                     ReactJS
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <FaSass />
                                     SASS
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <FaGit />
                                     GIT
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <SiWebdriverio />
                                     Webdriver.io
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <SiTypescript />
                                     Typescript
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <SiArduino />
                                     Arduino
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <SiSap />
                                     CAP
                                 </Item>
                             </Grid>
                             <Grid size={2} className="skill-list__span">
-                                <Item className={"skill-list__text " + (darkMode && "skill-list__text_dark")}>
+                                <Item className={"skill-list__text glass-background " + (darkMode && "skill-list__text_dark")}>
                                     <FaNode />
                                     NodeJS
                                 </Item>
